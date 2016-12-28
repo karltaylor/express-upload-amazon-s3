d00e09fdffd34d5f1f623778b9ecb3099b62a5f6
Karl Taylor <karl_taylor@me.com>
2016-12-28 20:15:09 +0000

    changing to standard js

--- app.js
+++ app.js
@@ -2,28 +2,28 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-	routes = require('./routes'),
-	multer = require('multer'),
-	upload = require('./routes/upload'),
-	http = require('http'),
-	path = require('path');
+const express = require('express')
+const routes = require('./routes')
+const multer = require('multer')
+const upload = require('./routes/upload')
+const http = require('http')
+const path = require('path')
 
-uploader = multer({
-	dest: 'uploads/'
-});
+var uploader = multer({
+  dest: 'uploads/'
+})
 
-var app = express();
+var app = express()
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'jade')
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', routes.index);
-app.post('/upload', uploader.single('singleFile'), upload.s3); //"singleFile" is the field name
+app.get('/', routes.index)
+app.post('/upload', uploader.single('singleFile'), upload.s3) // "singleFile" is the field name
 
-http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port http://localhost:' + app.get('port'))
+})
--- routes/upload.js
+++ routes/upload.js
@@ -1,45 +1,45 @@
- var _ = require('underscore'),
- 	AWS = require('aws-sdk'),
- 	fs = require('fs'),
- 	path = require('path'),
- 	flow = require('flow');
+const AWS = require('aws-sdk')
+const fs = require('fs')
+const path = require('path')
+const flow = require('flow')
 
- configPath = path.join(__dirname, '..', "config.json");
+const configPath = path.join(__dirname, '..', 'config.json')
+AWS.config.loadFromPath(configPath)
 
- AWS.config.loadFromPath(configPath);
+exports.s3 = function (req, res) {
+  var s3 = new AWS.S3()
+  var file = req.file
+  let result = {
+    error: 0,
+    uploaded: []
+  }
+  flow.exec(function () { // Read temp File
+    fs.readFile(file.path, this)
+  },
 
- exports.s3 = function(req, res) {
- 	var s3 = new AWS.S3(),
- 		file = req.file,
- 		result = {
- 			error: 0,
- 			uploaded: []
- 		};
+  function (err, data) { // Upload file to S3
+    if (err) console.log(err)
+    s3.putObject({
+      Bucket: 'your-bucket-name', // Bucket Name
+      Key: file.originalname, // Upload File Name, Default the original name
+      Body: data
+    }, this)
+  },
 
- 	flow.exec(
- 		function() { // Read temp File
- 			fs.readFile(file.path, this);
- 		},
- 		function(err, data) { // Upload file to S3
- 			s3.putObject({
- 				Bucket: 'XXXXX Bucket Name', //Bucket Name
- 				Key: file.originalname, //Upload File Name, Default the original name
- 				Body: data
- 			}, this);
- 		},
- 		function(err, data) { //Upload Callback
- 			if (err) {
- 				console.error('Error : ' + err);
- 				result.error++;
- 			}
- 			result.uploaded.push(data.ETag);
- 			this();
- 		},
- 		function() {
- 			res.render("result", {
- 				title: "Upload Result",
- 				message: result.error > 0 ? "Something is wroing, Check your server log" : "Success!!",
- 				entitiyIDs: result.uploaded
- 			});
- 		});
- };
+  function (err, data) { // Upload Callback
+    if (err) {
+      console.error('Error : ' + err)
+      result.error++
+    }
+    result.uploaded.push(data.ETag)
+    this()
+  },
+
+  function () {
+    res.render('result', {
+      title: 'Upload Result',
+      message: result.error > 0 ? 'Something is wroing, Check your server log' : 'Success!!',
+      entitiyIDs: result.uploaded
+    })
+  })
+}
